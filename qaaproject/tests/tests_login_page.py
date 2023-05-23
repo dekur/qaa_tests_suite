1bcdad33c091d5c5ef3f3eb9ac6553c95d0eab35
dekur <dekurdit@gmail.com>
2023-05-23 21:33:16 +0400

    added main project

---
+++ qaaproject/tests/tests_login_page.py
@@ -0,0 +1,47 @@
+import pytest
+from pages.catalog_page import CatalogPage
+from selenium import webdriver
+
+
+link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.mark.xfail
+def test_user_can_register(browser):
+    page = CatalogPage(browser, link)
+    page.open()
+    page.write_email_register()
+    page.write_password_register()
+    page.confirm_password_register()
+    page.click_on_register()
+    page.registration_successful()
+
+
+@pytest.mark.xfail
+def test_user_cant_register_with_incorrect_email(browser):
+    page = CatalogPage(browser, link)
+    page.open()
+    page.write_incorrect_email_register()
+    page.write_password_register()
+    page.confirm_password_register()
+    page.click_on_register()
+    page.registration_unsuccessful()
+
+
+@pytest.mark.login
+def test_user_can_login(browser):
+    page = CatalogPage(browser, link)
+    page.open()
+    page.write_email_login()
+    page.write_password_login()
+    page.click_on_login()
+    page.login_successful()
